22e42c22fbbdf211d10eab0e3d9bd6b6d6523c3f
evan hahn <evan.hahn@me.com>
2020-07-17 12:12:21 -0700

    fixed edit component

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const Pool = require("pg").Pool;
+const Pool = require('pg').Pool
 
 // const pool = new Pool({
 // 	user: 'postgres',
@@ -9,11 +9,11 @@ const Pool = require("pg").Pool;
 // })
 
 const pool = new Pool({
-  user: "norocallattfkm",
-  password: "bf13f97abf39847c80a8f6206a21b6e6099c8b1161ff9914ea3c66fcb20d8b72",
-  host: "ec2-34-225-162-157.compute-1.amazonaws.com",
-  port: 5432,
-  database: "detfrjs6u40o6o",
-});
+	user: 'norocallattfkm',
+	password: 'bf13f97abf39847c80a8f6206a21b6e6099c8b1161ff9914ea3c66fcb20d8b72',
+	host: 'ec2-34-225-162-157.compute-1.amazonaws.com',
+	port: 5432,
+	database: 'detfrjs6u40o6o',
+})
 
-module.exports = pool;
+module.exports = pool
