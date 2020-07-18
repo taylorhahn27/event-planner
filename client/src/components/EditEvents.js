634cb1190331447ab6bc2035dd39ed73a2f4e376
Taylor <taylorhahn27@gmail.com>
2020-07-18 14:28:20 -0700

    small edits

--- client/src/components/EditEvents.js
+++ client/src/components/EditEvents.js
@@ -1,9 +1,6 @@
 import React, { Fragment, useState } from 'react'
 import moment from 'moment'
 
-//TO DO:
-//editing text within edit event form is not working
-
 const EditEvents = ({ event }) => {
 	const [eventsData, setEvents] = useState(event)
 	const [events_name, setEventName] = useState(event.events_name)
